3735ee2c681d103269e6ae776d6121095bb56674
Marius Vitkevičius <marius@gyvenimas.org>
2014-07-22 14:57:25 +0300

    Code cleanup

--- BookmarksManager.Chrome/ChromeBookmarksReader.cs
+++ BookmarksManager.Chrome/ChromeBookmarksReader.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
