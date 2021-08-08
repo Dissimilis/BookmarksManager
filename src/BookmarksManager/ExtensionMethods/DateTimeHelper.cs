17d5de9af80cc27e03c7a655563eb99fedc86260
mkht <mkht@users.noreply.github.com>
2021-08-09 03:18:21 +0900

    Fix FromUnixTimeSeconds

--- src/BookmarksManager/ExtensionMethods/DateTimeHelper.cs
+++ src/BookmarksManager/ExtensionMethods/DateTimeHelper.cs
@@ -14,7 +14,7 @@ namespace BookmarksManager
             if (!unixTimeStamp.HasValue || unixTimeStamp < 1)
                 return null;
             unixTimeStamp = takeNDigits(unixTimeStamp.Value, 10);
-            return new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds((double)unixTimeStamp);
+            return DateTimeOffset.FromUnixTimeSeconds((long)unixTimeStamp).UtcDateTime;
         }
 
         public static DateTime? FromUnixTimeStamp(string unixTimeStamp)
@@ -32,7 +32,7 @@ namespace BookmarksManager
                 return number;
             int numberOfDigits = (int)Math.Floor(Math.Log10(number) + 1);
             if (numberOfDigits >= n)
-                return (int)Math.Truncate((number / Math.Pow(10, numberOfDigits - n)));
+                return (long)Math.Truncate((number / Math.Pow(10, numberOfDigits - n)));
             else
                 return number;
         }
